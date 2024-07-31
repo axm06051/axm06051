f8209975a889b674a6afefafe8797cb9038f2080
Adrian <adrian.mroz@grassvalley.com>
2024-07-31 14:44:01 +0200

    Create Microsoft.PowerShell_profile.ps1

---
+++ profiles/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+Import-Module Microsoft.PowerShell.SecretManagement
+Import-Module Microsoft.PowerShell.SecretStore
+
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile))
+{
+  Import-Module "$ChocolateyProfile"
+}
+
+Oh-My-Posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_modern.omp.json" | Invoke-Expression
